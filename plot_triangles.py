158a174728753067bdc143023e36091a7c6c2aff
Bill Agee <billagee@gmail.com>
2017-01-13 20:35:12 -0800

    Mostly working stopping point

--- plot_triangles.py
+++ plot_triangles.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python3.5
 
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.constants import golden
 
 def plot_equilateral_triangle(
         sidelength, index=0, lastlines=None,
-        hinge="right", invert=False):
-    # Find equilateral triangle height by halving it and finding the long side # of the resulting right triangle.
-    # See Google pythagorean calc search:
-    #   a = sqrt(c^2 - b^2)
-    #   pythagorean theorem calc: find a, b=n/a, c=n/a
-    #   https://www.google.com/search?q=pythagorean+theorem+calc%3A+find+a%2C+b%3Dn%2Fa%2C+c%3Dn%2Fa
-    height = np.sqrt([ sidelength**2.0 - (sidelength / 2.0) ])[0]
+        hinge="right", invert=False, fillcolor=None):
+    # Find equilateral triangle height:
+    height = math.sqrt(3.0) * (sidelength / 2.0)
 
-    # (x1,y1) is the triangle's left vertex, (x2,y2) the right, (x3,y3) the top
-    startx = starty = None
     print("Plotting triangle {} starting at {} of old one".format(index, hinge))
-    # Start points will always be third vertex plotted in prev triangle
+    startx = starty = None
+    # Start points will always be third vertex plotted in the prev triangle
     startx = lastlines[0].get_xdata()[2]
     starty = lastlines[0].get_ydata()[2]
     # Plot dot at start point
     #plt.plot([startx], [starty], 'ro')
     if hinge == "left":
-        # Triangles started from left of prev get their vertexes
+        # Triangles started from left of prev get their vertices
         # plotted in this order:
         # R -> L -> T
         if invert:
@@ -41,7 +37,7 @@ def plot_equilateral_triangle(
             x3 = startx - (sidelength / 2.0) # top
             y3 = starty + height # top
     elif hinge == "right":
-        # Triangles started from right of prev get their vertexes
+        # Triangles started from right of prev get their vertices
         # plotted in this order:
         # T -> R -> L
         if invert:
@@ -59,10 +55,9 @@ def plot_equilateral_triangle(
             x3 = startx - (sidelength / 2.0) # left
             y3 = starty - height # left
     elif hinge == "top":
-        # Triangles started from top of prev get their vertexes
+        # Triangles started from top of prev get their vertices
         # plotted in this order:
         # L -> T -> R
-        #import pdb ; pdb.set_trace()
         if invert:
             x1 = startx # left
             y1 = starty # left
@@ -79,7 +74,9 @@ def plot_equilateral_triangle(
             y3 = starty # right
     else:
         raise Exception("Bad hinge point {}".format(hinge))
-    lines = ax.plot(np.array([x1, x2, x3, x1]), np.array([y1, y2, y3, y1])) #, linewidth=2.0)
+    lines = ax.plot(np.array([x1, x2, x3, x1]), np.array([y1, y2, y3, y1]), linewidth=1.0, color="b")
+    if fillcolor:
+        plt.fill(np.array([x1, x2, x3, x1]), np.array([y1, y2, y3, y1]), fillcolor)
     return lines
 
 #plt.ion() # Interactively display updates as they're plot()ted
@@ -100,11 +97,11 @@ ax.grid(True)
 """
 
 # Special case of first triangle
-sidelength = 1.0
-#prev_sidelengths = [1.0]
-height = np.sqrt([ sidelength**2.0 - (sidelength / 2.0) ])[0]
+sidelength = 5.0
+prev_sidelengths = [5.0]
+height = math.sqrt(3.0) * (sidelength / 2.0)
 # Plot in order: L -> T -> R
-startx = starty = 0
+startx = starty = 1
 x1 = startx + sidelength # right
 y1 = starty # right
 x2 = startx + (sidelength / 2.0) # top
@@ -112,11 +109,12 @@ y2 = starty - height # top
 x3 = startx # left
 y3 = starty # left
 #plt.plot([x1], [y1], 'ro')
-lastlines = ax.plot(np.array([x1, x2, x3, x1]), np.array([y1, y2, y3, y1])) #, linewidth=2.0)
+lastlines = ax.plot(np.array([x1, x2, x3, x1]), np.array([y1, y2, y3, y1]))
+plt.fill(np.array([x1, x2, x3, x1]), np.array([y1, y2, y3, y1]), "#666666")
 
 # Plot remaining triangles in a loop
 hinge = "right"
-for i in range(0, 9):
+for i in range(0, 7):
     if i % 2 == 0:
         invert = False
     else:
@@ -126,10 +124,57 @@ for i in range(0, 9):
             index=i,
             lastlines=lastlines,
             hinge=hinge,
-            invert=invert)
-    #prev_sidelengths.append(sidelength)
-    #sidelength = sidelength + prev_sidelengths[-2]
-    sidelength = sidelength * golden
+            invert=invert,
+            fillcolor='#666666')
+
+    prev_sidelengths.append(sidelength)
+    sidelength = sidelength + prev_sidelengths[-2]
+    #sidelength = sidelength * golden
+
+    # Toggle hinge from right -> left -> top -> ...
+    if hinge == "right":
+        hinge = "left"
+    elif hinge == "left":
+        hinge = "top"
+    elif hinge == "top":
+        hinge = "right"
+
+# First triangle of second batch
+sidelength = 5.0
+prev_sidelengths = [5.0]
+height = math.sqrt(3.0) * (sidelength / 2.0)
+# Plot in order: L -> T -> R
+startx = 2.5
+starty = 1 - height
+x1 = startx # left
+y1 = starty # left
+x2 = startx + (sidelength / 2.0) # top
+y2 = starty + height # top
+x3 = startx + sidelength # right
+y3 = starty # right
+#plt.plot([x1], [y1], 'ro')
+lastlines = ax.plot(np.array([x1, x2, x3, x1]), np.array([y1, y2, y3, y1]), color="#35206f")
+plt.fill(np.array([x1, x2, x3, x1]), np.array([y1, y2, y3, y1]), "#35206f")
+
+# Second batch
+hinge = "right"
+for i in range(0, 7):
+    if i % 2 == 0:
+        invert = True
+    else:
+        invert = False
+    lastlines = plot_equilateral_triangle(
+            sidelength,
+            index=i,
+            lastlines=lastlines,
+            hinge=hinge,
+            invert=invert,
+            fillcolor='#35206f')
+
+    prev_sidelengths.append(sidelength)
+    sidelength = sidelength + prev_sidelengths[-2]
+    #sidelength = sidelength * golden
+
     # Toggle hinge from right -> left -> top -> ...
     if hinge == "right":
         hinge = "left"
